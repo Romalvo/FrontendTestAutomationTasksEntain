9e7e4f7f795c6b8528be536a69819b3d1378ea2f
Romalvo <roman.voronkov.06@gmail.com>
2024-12-11 08:56:17 +0200

    Created viewports.

---
+++ cypress/fixtures/viewports.js
@@ -0,0 +1,16 @@
+module.exports = {
+    desktop: {
+        width:1280,
+        height: 720
+    },
+
+    mobile: {
+        width: 375,
+        height: 667
+    },
+
+    tablet: {
+        width: 768,
+        height: 1024
+    }
+ };
